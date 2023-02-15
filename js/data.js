cc7cbefec50ded977e82f0dae85a583ac612f717
Tigran Mkrtchyan <tigranmkrtchyan59@gmail.com>
2023-02-15 07:57:25 +0000

    Completed issue-1-create-an-entry

--- js/data.js
+++ js/data.js
@@ -1,8 +1,18 @@
 /* exported data */
-
+function beforeUnload(event) {
+  var dataJSON = JSON.stringify(data);
+  localStorage.setItem('data-local-storage', dataJSON);
+}
 var data = {
   view: 'entry-form',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
+
+var FirstDataJSON = localStorage.getItem('data-local-storage');
+if (FirstDataJSON !== null) {
+  data = JSON.parse(FirstDataJSON);
+}
+
+window.addEventListener('beforeunload', beforeUnload);
